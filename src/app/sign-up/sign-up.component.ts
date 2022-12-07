f336d9ce3e9bb76fa5f01ca28cee11bc77df29d0
Bertrand Briset <82585037+BBriset@users.noreply.github.com>
2022-12-07 14:53:32 +0100

    FormQuest solution

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css'],
+})
+export class SignUpComponent {
+  user: User = new User('', '', '');
+  onSubmit() {
+    console.log(this.user);
+  }
+}
